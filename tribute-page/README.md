6735ad74aad7f57598a3e811a66a11c25c799016
SRM19 <swathirm99@gmail.com>
2022-07-22 15:36:00 +0200

    first commit

---
+++ tribute-page/README.md
@@ -0,0 +1,4 @@
+# Tribute Page :)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/SRM19/pen/PQRjWR](https://codepen.io/SRM19/pen/PQRjWR).
+
---
+++ tribute-page/src/script.js
@@ -0,0 +1,14 @@
+  $(document).ready(function() {
+    $(".fa-heartbeat").addClass("animated infinite pulse");
+    
+   $(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
+  });
